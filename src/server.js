f8a00f39c6f71db4e7c78fe509b00bca6bab465b
JacopoDaeli <jacopo.daeli@gmail.com>
2017-01-19 13:24:34 +0100

    Awesomeeee

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const http = require('http')
 const server = http.createServer((request, response) => {
   console.log(`Received request for URL: ${request.url}`)
   response.writeHead(200)
-  response.end('Hello from Codito!')
+  response.end('Hello guys! You are awesome!')
 })
 
 server.listen(8080)
